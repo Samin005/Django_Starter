8c1113b514f7afe162d8d392a662dce3485cc941
Samin Azhan <samin005.bracu@gmail.com>
2020-06-10 03:49:20 +0600

    Added test cases for views.

--- polls/views.py
+++ polls/views.py
@@ -31,12 +31,18 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.filter(
             pub_date__lte=timezone.now()
-        )[:5]
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):

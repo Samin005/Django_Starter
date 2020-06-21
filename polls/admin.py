b5dcd79f7b87a14fc076d8706d82ec482cdba0ed
Samin Azhan <samin005.bracu@gmail.com>
2020-06-22 00:49:20 +0600

    Added 3 choices per questions in admin panel.

--- polls/admin.py
+++ polls/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
-    # list_display = ('question_text', 'pub_date')
-    list_display = list(Question().__dict__.keys())[1:]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # list_display = list(Question().__dict__.keys())[1:]
+    inlines = [ChoiceInline]
 
 
 class ChoiceAdmin(admin.ModelAdmin):

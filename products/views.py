4bf6d0a257baf6b28631b0569fc836d5f044b3b5
Samin Azhan <samin005.bracu@gmail.com>
2020-07-01 22:11:14 +0600

    Modified the built-in update method of products to make sure only the creator of the product can update the product.

--- products/views.py
+++ products/views.py
@@ -2,14 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product, Offer
 
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 from .serializers import ProductSerializer, OfferSerializer
 
 
 class ProductsAPIAll(viewsets.ModelViewSet):
     queryset = Product.objects.all().order_by('name')
     serializer_class = ProductSerializer
-    permission_classes = [permissions.DjangoModelPermissions]
+    permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
+
+    def update(self, request, *args, **kwargs):
+        product_creator_user_id = 2
+        if request.user.id != product_creator_user_id:
+            return Response(status=status.HTTP_401_UNAUTHORIZED, data={
+                'detail': f'You are not allowed to update product {self.get_object().name}({self.get_object().id})'
+            })
+        else:
+            return super(ProductsAPIAll, self).update(request)
 
 
 class OffersAPIAll(viewsets.ModelViewSet):
